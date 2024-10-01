757452a51892d7abc4c43917ed43fee6bb7fa11f
rashedrk <rashedrk001@gmail.com>
2024-10-01 22:05:12 +0600

    task-9

--- uniqueValues.js
+++ uniqueValues.js
@@ -1,4 +1,4 @@
-// 8. Task: Unique Values
+// 9. Task: Unique Values
 // ----------------------------------
 // Create an array of numbers with some duplicate values. Write a function to filter
 // out the duplicate values and return a new array with only unique numbers. 
