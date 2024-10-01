26ea7282123fc989bc0b73f946557a9639af08c9
rashedrk <rashedrk001@gmail.com>
2024-10-01 22:04:06 +0600

    task-8

---
+++ uniqueValues.js
@@ -0,0 +1,14 @@
+// 8. Task: Unique Values
+// ----------------------------------
+// Create an array of numbers with some duplicate values. Write a function to filter
+// out the duplicate values and return a new array with only unique numbers. 
+// Print the result.
+
+const numbers = [1, 2, 5, 10, 2, 10, 5, 9, 1, 5];
+
+const uniqueValues = (arr) => {
+    const unique = arr.filter((value, index) => arr.indexOf(value) === index);
+    return unique;
+}
+
+console.log(uniqueValues(numbers));
