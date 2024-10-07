5ffa220ae07c4539a295c742e5f0a43fdeaf049c
rashedrk <rashedrk001@gmail.com>
2024-10-07 15:37:23 +0600

    task-12

--- ReduceFunction.js
+++ ReduceFunction.js
@@ -1,4 +1,4 @@
-// Task: Functional Programming - Reduce
+// 12. Task: Functional Programming - Reduce
 // Write a function that uses the reduce function to calculate the total value of an array
 // of objects with a 'quantity' and 'price' property.
 const items = [
